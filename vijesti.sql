c483b7ae6fef18553deadb3ffbae5e6e16e35557
PhpGenij <luka.antolic32@gmail.com>
2024-06-13 23:52:47 +0200

    Update vijesti.sql

--- vijesti.sql
+++ vijesti.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2024 at 02:30 PM
+-- Generation Time: Jun 13, 2024 at 11:50 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `vijesti`
 --
+CREATE DATABASE IF NOT EXISTS `vijesti` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `vijesti`;
 
 -- --------------------------------------------------------
 
